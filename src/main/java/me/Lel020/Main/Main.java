650a5fcc33b459a6ade1c9f0bde60b622a92e8d3
chavapa <mahmud.jawaad03@gmail.com>
2023-03-12 21:05:33 +1100

    Name now has an Input-Validation

--- src/main/java/me/Lel020/Main/Main.java
+++ src/main/java/me/Lel020/Main/Main.java
@@ -4,6 +4,7 @@ import me.Lel020.Users.UserManager;
 
 public class Main {
     public static void main(String[] args) {
+
         UserManager userManager = new UserManager();
         userManager.addUser();
     }
--- src/main/java/me/Lel020/Users/UserManager.java
+++ src/main/java/me/Lel020/Users/UserManager.java
@@ -10,31 +10,50 @@ public class UserManager {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+        String name = null;
+        String email = null;
+
+        boolean isName = false;
+        boolean isEmail = false;
+
+        do{
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+            isName = isValidName(name);
+
+            if(!isName){
+                System.out.println("Invalid name format, please try again: ");
+            }
+        }
+        while (!isName);
 
         System.out.println("Enter a password: ");
         String password = scanner.nextLine();
 
-        boolean isValid = false;
         do {
             System.out.println("Enter your email: ");
-            String email = scanner.nextLine();
-            isValid = isValidEmail(email);
+            email = scanner.nextLine();
+            isEmail = isValidEmail(email);
 
-            if(!isValid){
-                System.out.println("Invalid email!");
-            } else {
-                users.add(new User(name, password, email));
-                System.out.println("User added!");
+            if(!isEmail){
+                System.out.println("Invalid email, please try again: ");
             }
         }
-        while(!isValid);
+        while(!isEmail);
+
+        users.add(new User(name,password, email));
+        System.out.println("User added!");
+
         scanner.close();
     }
 
     public boolean isValidEmail(String email){
         String regex = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
         return email.matches(regex);
+    }
+
+    public boolean isValidName(String name){
+        String regex = "^[A-za-z]+([\\s'-][A-Za-z]+)$";
+        return name.matches(regex);
     }
 }
