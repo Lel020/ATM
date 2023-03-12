94169a5c4adff1d7506b8875e6b2774b0cf6da30
chavapa <mahmud.jawaad03@gmail.com>
2023-03-12 18:12:09 +1100

    Tested UserManager via Main.java

--- src/main/java/me/Lel020/Main/Main.java
+++ src/main/java/me/Lel020/Main/Main.java
@@ -1,6 +1,10 @@
 package me.Lel020.Main;
 
+import me.Lel020.Users.UserManager;
+
 public class Main {
     public static void main(String[] args) {
+        UserManager userManager = new UserManager();
+        userManager.addUser();
     }
 }
