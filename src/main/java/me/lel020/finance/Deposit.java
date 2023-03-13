83289ee24c25d3ce774b6fb3f8031aacbeaa6acb
chavapa <mahmud.jawaad03@gmail.com>
2023-03-13 19:16:49 +1100

    Added Deposit.java

--- src/main/java/me/lel020/finance/Deposit.java
+++ src/main/java/me/lel020/finance/Deposit.java
@@ -1,4 +1,11 @@
 package me.lel020.finance;
 
+import java.util.HashSet;
 public class Deposit {
+
+    HashSet<Double> deposit = new HashSet<Double>();
+
+    public void depositAmount(double amount){
+        deposit.add(amount);
+    }
 }
