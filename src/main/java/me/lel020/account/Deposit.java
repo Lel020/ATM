533b2a7b7d55543f83f5d2d869b94befa47a75a6
lel020 <mahmud.jawaad03@gmail.com>
2023-03-20 09:28:21 +1100

    Added javadoc for Deposit.java

--- src/main/java/me/lel020/account/Deposit.java
+++ src/main/java/me/lel020/account/Deposit.java
@@ -5,8 +5,11 @@ public class Deposit {
 
     HashSet<Double> deposit = new HashSet<Double>();
 
+    /**
+     * Adds a deposit amount to the set.
+     * @param amount - amount to be deposited.
+     */
     public void depositAmount(double amount){
-
         deposit.add(amount);
     }
 }
