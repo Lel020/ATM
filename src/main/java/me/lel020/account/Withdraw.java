3d0a46cc2efb221ac9c7f21d5a3051e2c11303db
lel020 <mahmud.jawaad03@gmail.com>
2023-03-20 09:49:59 +1100

    Added javadoc for Withdraw.java

--- src/main/java/me/lel020/account/Withdraw.java
+++ src/main/java/me/lel020/account/Withdraw.java
@@ -6,6 +6,10 @@ public class Withdraw {
 
     HashSet<Double> withdraw = new HashSet<Double>();
 
+    /**
+     * Adds a withdrawal amount to the set.
+     * @param amount - amount to be withdrawed.
+     */
     public void withdrawAmount(double amount) {
         if(amount <= 0){
             System.out.println("ERROR: You have $0 in the bank.");
