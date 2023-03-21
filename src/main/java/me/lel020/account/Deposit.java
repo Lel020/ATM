73050bbd8c6b24d7a7d9a9a8a5202636959db7b3
lel020 <mahmud.jawaad03@gmail.com>
2023-03-21 20:59:31 +1100

    Fixed error, replaced if() with while()

--- src/main/java/me/lel020/account/Deposit.java
+++ src/main/java/me/lel020/account/Deposit.java
@@ -1,18 +1,23 @@
 package me.lel020.account;
 
 import java.util.HashSet;
+import java.util.Scanner;
+
 public class Deposit {
 
     HashSet<Double> deposit = new HashSet<Double>();
+    Scanner scanner = new Scanner(System.in);
 
     /**
      * Adds a deposit amount to the set.
      * @param amount - amount to be deposited.
      */
     public void depositAmount(double amount){
-        if(amount > 10_000){
+        while(amount > 10_000){
             System.out.println("You can only deposit up to $10,000");
+            System.out.println("Please enter an amount <= $10,000");
+            amount = scanner.nextDouble();
+            deposit.add(amount);
         }
-        deposit.add(amount);
     }
 }
